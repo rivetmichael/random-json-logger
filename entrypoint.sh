7f45755edc01eacc2c7fb2cdaaebcab2d3a78f7f
Michael R <rivetmichael@users.noreply.github.com>
2020-03-09 14:25:51 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,15 @@ do
    sleep $waitTime &
    wait $!
    instruction=$(shuf -i 0-4 -n 1)
-   d=`date -Iseconds`
+   d=`date +%d/%b/%Y:%H:%M:%S\ %z`
    case "$instruction" in
-      "1") echo "{\"@timestamp\": \"$d\", \"level\": \"ERROR\", \"message\": \"something happened in this execution.\"}"
+      "1") echo "{\"time\": \"$d\", \"level\": \"ERROR\", \"message\": \"something happened in this execution.\"}"
       ;;
-      "2") echo "{\"@timestamp\": \"$d\", \"level\": \"INFO\", \"message\": \"takes the value and converts it to string.\"}"
+      "2") echo "{\"time\": \"$d\", \"level\": \"INFO\", \"message\": \"takes the value and converts it to string.\"}"
       ;;
-      "3") echo "{\"@timestamp\": \"$d\", \"level\": \"WARN\", \"message\": \"variable not in use.\"}"
+      "3") echo "{\"time\": \"$d\", \"level\": \"WARN\", \"message\": \"variable not in use.\"}"
       ;;
-      "4") echo "{\"@timestamp\": \"$d\", \"level\": \"DEBUG\", \"message\": \"first loop completed.\"}"
+      "4") echo "{\"time\": \"$d\", \"level\": \"DEBUG\", \"message\": \"first loop completed.\"}"
       ;;
    esac
 done
